83930bee758c29bd12dd4393ce6a7b38ab815b67
Oli Dacombe <olidacombe@gmail.com>
2023-12-02 22:37:05 +0000

    01-2

--- aoc2023-01-2/src/lib.rs
+++ aoc2023-01-2/src/lib.rs
@@ -24,34 +24,32 @@ fn stats_with_digit_repr(s: &str) -> Option<u32> {
     static DIGITS: OnceLock<HashMap<&str, u32>> = OnceLock::new();
     DIGITS
         .get_or_init(|| {
-            HashMap::from(
-                [
-                    ("0", 0),
-                    ("1", 1),
-                    ("2", 2),
-                    ("3", 3),
-                    ("4", 4),
-                    ("5", 5),
-                    ("6", 6),
-                    ("7", 7),
-                    ("8", 8),
-                    ("9", 9),
-                    ("zero", 0),
-                    ("one", 1),
-                    ("two", 2),
-                    ("three", 3),
-                    ("four", 4),
-                    ("five", 5),
-                    ("six", 6),
-                    ("seven", 7),
-                    ("eight", 8),
-                    ("nine", 9),
-                ], // .map(|(k, v)| (k.to_string(), v)),
-            )
+            HashMap::from([
+                ("0", 0),
+                ("1", 1),
+                ("2", 2),
+                ("3", 3),
+                ("4", 4),
+                ("5", 5),
+                ("6", 6),
+                ("7", 7),
+                ("8", 8),
+                ("9", 9),
+                ("zero", 0),
+                ("one", 1),
+                ("two", 2),
+                ("three", 3),
+                ("four", 4),
+                ("five", 5),
+                ("six", 6),
+                ("seven", 7),
+                ("eight", 8),
+                ("nine", 9),
+            ])
         })
         .iter()
         .find(|(match_me, _)| s.starts_with(*match_me))
-        .map(|(k, v)| v)
+        .map(|(_, v)| v)
         .copied()
 }
 
@@ -77,8 +75,8 @@ fn calibration(v: &str) -> Result<u32> {
     Ok(10 * first_digit(v)? + last_digit(v)?)
 }
 
-pub fn sum_calibration<'a>(it: impl Iterator<Item = &'a str>) -> Result<u32> {
-    it.filter_map(|line| calibration(line).ok())
+pub fn sum_calibration<'a>(it: impl Iterator<Item = String>) -> Result<u32> {
+    it.filter_map(|line| calibration(&line).ok())
         .reduce(|acc, v| acc + v)
         .ok_or(eyre!("Some calibration summing error 🤷"))
 }
@@ -119,7 +117,7 @@ mod tests {
             zoneight234
             7pqrstsixteen
         "};
-        assert_eq!(sum_calibration(example.lines())?, 281);
+        assert_eq!(sum_calibration(example.lines().map(String::from))?, 281);
         Ok(())
     }
 }
