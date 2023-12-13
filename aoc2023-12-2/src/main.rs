137da41b693fc63009976f2d625f5c889b07c80b
Oli Dacombe <olidacombe@gmail.com>
2023-12-13 22:40:04 +0000

    12-2 naive approach

---
+++ aoc2023-12-2/src/main.rs
@@ -0,0 +1,9 @@
+use std::io;
+
+use aoc2023_12_2::sum_possible_arrangements;
+
+fn main() {
+    let lines = io::stdin().lines().filter_map(|l| l.ok());
+    let answer = sum_possible_arrangements(lines);
+    println!("Answer: {answer}");
+}
