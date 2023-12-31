1fa9f16e2dc6e74e1825651aad933d87d051131b
Oli Dacombe <oliver.dacombe@gsr.io>
2024-01-01 01:35:45 +0200

    don't like the way this is going

--- aoc2023-18-1/src/lib.rs
+++ aoc2023-18-1/src/lib.rs
@@ -3,33 +3,61 @@ use nom::{
     bytes::complete::tag,
     character::complete::{alpha1, digit1, u32},
     character::complete::{hex_digit1, multispace1},
-    sequence::{delimited, pair, separated_pair, tuple},
+    sequence::{delimited, separated_pair},
     IResult,
 };
 
-enum Step {
-    R(u32),
-    U(u32),
-    L(u32),
-    D(u32),
+#[derive(Clone, PartialEq, Eq)]
+enum Direction {
+    R,
+    U,
+    L,
+    D,
 }
 
-fn parse_step(input: &str) -> IResult<&str, (&str, u32)> {
-    separated_pair(
-        alt((tag("R"), tag("U"), tag("L"), tag("D"))),
-        multispace1,
-        u32,
-    )(input)
+impl Direction {
+    pub fn flipped(self) -> Self {
+        match self {
+            Direction::R => Direction::L,
+            Direction::U => Direction::D,
+            Direction::L => Direction::R,
+            Direction::D => Direction::U,
+        }
+    }
+
+    pub fn parallel(&self, rhs: &Self) -> i8 {
+        match self {
+            Direction::R => match rhs {
+                Direction::R => 1,
+                Direction::L => -1,
+                _ => 0,
+            },
+            Direction::U => match rhs {
+                Direction::U => 1,
+                Direction::D => -1,
+                _ => 0,
+            },
+            Direction::L => match rhs {
+                Direction::L => 1,
+                Direction::R => -1,
+                _ => 0,
+            },
+            Direction::D => match rhs {
+                Direction::D => 1,
+                Direction::U => -1,
+                _ => 0,
+            },
+        }
+    }
 }
 
-impl From<&str> for Step {
+impl From<&str> for Direction {
     fn from(value: &str) -> Self {
-        let (direction, length) = parse_step(value).unwrap().1;
-        match direction {
-            "R" => Step::R(length),
-            "U" => Step::U(length),
-            "L" => Step::L(length),
-            "D" => Step::D(length),
+        match value {
+            "R" => Direction::R,
+            "U" => Direction::U,
+            "L" => Direction::L,
+            "D" => Direction::D,
             _ => unreachable!(),
         }
     }
@@ -51,7 +79,8 @@ impl From<&str> for Colour {
 }
 
 struct Instruction {
-    step: Step,
+    direction: Direction,
+    length: usize,
     colour: Colour,
 }
 
@@ -67,9 +96,61 @@ impl From<String> for Instruction {
     fn from(value: String) -> Self {
         let ((direction, length), colour) = parse_instruciton(value.as_str()).unwrap().1;
         Self {
-            step: Step::from(format!("{direction} {length}").as_str()),
+            direction: Direction::from(direction),
+            length: length.parse().unwrap(),
             colour: Colour::from(colour),
         }
+    }
+}
+
+struct Rover {
+    x: usize,
+    y: usize,
+    last_direction: Option<Direction>,
+    boundary_stack: Vec<usize>,
+}
+
+impl Rover {
+    fn boundary_coord(&self) -> usize {
+        match self.last_direction.as_ref().unwrap() {
+            Direction::U | Direction::D => self.y,
+            Direction::R | Direction::L => self.x,
+        }
+    }
+
+    pub fn mv(&mut self, instruction: Instruction) {
+        if self.last_direction.is_none() {
+            self.last_direction = Some(instruction.direction.clone());
+        }
+        if instruction.direction == Direction::U && instruction.length > self.y {
+            if self
+                .last_direction
+                .as_ref()
+                .unwrap()
+                .parallel(&instruction.direction)
+                != 0
+            {
+                let diff = instruction.length.abs_diff(self.y);
+                self.boundary_stack.iter_mut().for_each(|i| *i = *i + diff);
+                self.y += diff;
+            }
+        } else if instruction.direction == Direction::L && instruction.length > self.x {
+            if self
+                .last_direction
+                .as_ref()
+                .unwrap()
+                .parallel(&instruction.direction)
+                != 0
+            {
+                let diff = instruction.length.abs_diff(self.x);
+                self.boundary_stack.iter_mut().for_each(|i| *i = *i + diff);
+                self.x += diff;
+            }
+        }
+        if self.last_direction.as_ref().unwrap().parallel(&instruction.direction) == 1 {
+        }
+        else if self.last_direction.as_ref().unwrap().parallel(&instruction.direction) == -1 {
+        self.boundary_stack.push(self.boundary_coord());
     }
 }
 
