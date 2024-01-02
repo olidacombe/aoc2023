4fcd566a90f7df753b6a9cd6ffc926202c859693
Oli Dacombe <oliver.dacombe@gsr.io>
2024-01-02 23:54:20 +0200

    start sketching

--- aoc2023-18-2/src/lib.rs
+++ aoc2023-18-2/src/lib.rs
@@ -1,4 +1,93 @@
-pub fn cubic_metres_of_lava(mut it: impl Iterator<Item = String>) -> usize {
+use std::ops::{RangeFrom, RangeFull, RangeToInclusive, Sub};
+
+enum Range<T> {
+    Full(RangeFull),
+    RangeToInclusive(RangeToInclusive<T>),
+    RangeFrom(RangeFrom<T>),
+    RangeInclusive(std::ops::RangeInclusive<T>),
+}
+
+impl Range<i64> {
+    fn size(&self) -> Option<usize> {
+        match self {
+            Range::RangeInclusive(range) => Some((*range.end() - *range.start()).abs() as usize),
+            _ => None,
+        }
+    }
+}
+
+struct Limits<T = i64> {
+    h: Range<T>,
+    v: Range<T>,
+}
+
+trait Area {
+    fn area(&self) -> Option<usize>;
+}
+
+impl Area for Limits<i64> {
+    fn area(&self) -> Option<usize> {
+        self.v
+            .size()
+            .zip(self.h.size())
+            .map(|(height, width)| height * width)
+    }
+}
+
+enum Region {
+    // Unknown
+    U(Limits),
+    // Left of path
+    L(Limits),
+    // Right of path
+    R(Limits),
+}
+
+impl Area for Region {
+    fn area(&self) -> Option<usize> {
+        match self {
+            Region::U(limits) | Region::L(limits) | Region::R(limits) => limits.area(),
+        }
+    }
+}
+
+enum Instruction {
+    R(usize),
+    U(usize),
+    L(usize),
+    D(usize),
+}
+
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+struct PathSegment {
+    from: Point,
+    instruction: Instruction,
+}
+
+trait RegionSplitter {
+    fn split(self) -> Vec<Region>;
+}
+
+impl RegionSplitter for Region {
+    fn split(self) -> Vec<Region> {
+        todo! {}
+    }
+}
+
+impl RegionSplitter for Vec<Region> {
+    fn split(self) -> Vec<Region> {
+        self.into_iter()
+            .map(RegionSplitter::split)
+            .flatten()
+            .collect()
+    }
+}
+
+pub fn cubic_metres_of_lava(it: impl Iterator<Item = String>) -> usize {
     usize::default()
 }
 
